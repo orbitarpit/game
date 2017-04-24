a550be5d6b32e12bae733ed452b1b459ded27d8d
orbitarpit <orbitarpit@users.noreply.github.com>
2017-04-25 03:21:16 +0530

    Update puzzles.js

--- js/puzzles.js
+++ js/puzzles.js
@@ -2,17 +2,17 @@
 // DOTS
 ////////////////////////////////////////////////////////////
 
-var puzzles_arr = [{dots:[  {x:500, y:103},{x:450, y:203},{x:410, y:303},{x:370, y:413},{x:333, y:533},{x:290, y:640}, ],
+var puzzles_arr = [{dots:[  {x:500, y:103},{x:333, y:533},{x:290, y:640}, ],
 				src:'assets/A1.png',
 				category:'LETTERS',
 				timer:1500000,
 				joinLastDot:false},
-		{dots:[  {x:500, y:103},{x:570, y:203},{x:610, y:303},{x:650, y:413},{x:690, y:533},{x:720, y:640}, ],
+		{dots:[  {x:500, y:103},{x:690, y:533},{x:720, y:640}, ],
 				src:'assets/A2.png',
 				category:'LETTERS',
 				timer:1500000,
 				joinLastDot:false},
-		{dots:[  {x:320, y:520},{x:460, y:520},{x:570, y:520},{x:680, y:520}, ],
+		{dots:[  {x:320, y:520},{x:680, y:520}, ],
 				src:'assets/A3.png',
 				category:'LETTERS',
 				timer:1500000,
